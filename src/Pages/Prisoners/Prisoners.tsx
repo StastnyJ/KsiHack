9d46c8fcc861322af2addf437d2bcabf96ca6d82
StastnyJ <xstastnyj@gmail.com>
2021-12-17 00:04:13 +0100

    ~ add prisoner details

--- src/Pages/Prisoners/Prisoners.tsx
+++ src/Pages/Prisoners/Prisoners.tsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Container, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Tooltip } from "@mui/material";
-import { FileDownload } from "@mui/icons-material";
+import {
+  Container,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  Button,
+} from "@mui/material";
+import { FileDownload, InfoOutlined } from "@mui/icons-material";
 import { apiFetch, apiUrl } from "../../Utils/Api";
 
 interface Prisoner {
@@ -11,6 +25,7 @@ interface Prisoner {
 
 export default function Prisoners() {
   const [prisoners, setPrisoners] = useState<Prisoner[]>([]);
+  const [modalOpened, setModalOpened] = useState(false);
 
   useEffect(() => {
     apiFetch(
@@ -27,14 +42,19 @@ export default function Prisoners() {
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell>Id</TableCell>
             <TableCell>Jméno</TableCell>
             <TableCell>Zločin</TableCell>
-            <TableCell style={{ textAlign: "right" }}>Detailní informace</TableCell>
+            <TableCell style={{ textAlign: "right" }}>
+              Detailní informace{" "}
+              <InfoOutlined onClick={() => setModalOpened(true)} style={{ cursor: "pointer" }} color="info" />
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {prisoners.map((p) => (
             <TableRow key={p.id}>
+              <TableCell>{p.id}</TableCell>
               <TableCell>{p.name}</TableCell>
               <TableCell>{p.crime}</TableCell>
               <TableCell style={{ textAlign: "right" }}>
@@ -51,6 +71,18 @@ export default function Prisoners() {
           ))}
         </TableBody>
       </Table>
+      <Dialog maxWidth="lg" open={modalOpened} onClose={() => setModalOpened(false)}>
+        <DialogTitle>Informace o detailech</DialogTitle>
+        <DialogContent>
+          Detaily jsou uložené v souboru, který je zašifrovaný pomocí 8 Bytového klíče DES algoritmem v ECB módu. První 2 Byty
+          klíče odpovídají id vězňa zakódovaného v bigEndian formátu. Další 3 Byty jsou bezpečnostní Byty, které byly zvoleny
+          administrátorem aplikace v době vězňovi registrace. Poslední 3 Byty klíče odpovídají prvním 3 znakům vězňova jména v
+          ASCII formátu.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setModalOpened(false)}>Zavřít</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
